bb860cc16fe6a382f8ab0e761799776fa81ec4ce
Tom Emmerson <thomasemmerson@users.noreply.github.com>
2017-01-12 13:57:15 +0000

    New

---
+++ kata/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata
+{
+    public class readDictionary
+    {
+        Dictionary<int, string> newDictionary = new Dictionary<int, string>();
+
+        public void ReadIt()
+        {
+            // Open the file into a streamreader
+            using (StreamReader sr = new StreamReader("dictionary.txt"))
+            {
+                int i = 0;
+                while (!sr.EndOfStream) // Keep reading until we get to the end
+                {
+                    //read each line
+                    string word = sr.ReadLine();
+
+                    //add it to the dictionary
+                    newDictionary.Add(i, word);
+
+                    //iterate
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ kataUnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kata;
+using NUnit;
+
+namespace kataUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
